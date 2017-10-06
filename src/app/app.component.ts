46f939b8f20183d16e8917c118e4f6b273a580ca
Jan Kucera <jan.nekucera@gmail.com>
2017-10-07 00:01:21 +0200

    zakladni vykresleni dat z Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,25 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App juch';  
-  playgrounds: Observable<any[]>;
+  title = 'Rezervace'; 
+  today = '20160219'; 
+  arenas: [{
+    id: number,
+    name: String,
+    hours: Observable<any[]>
+  }]; 
+  hours = [];
 
-  constructor(db: AngularFireDatabase) { 
-  	this.playgrounds = db.list('/playground/20160219').valueChanges();
+  constructor(db: AngularFireDatabase) {
+    for (var i = 0; i < 24; i++) { this.hours.push(i);}
+    this.arenas = [{
+      id: 1,
+      name: "Sprint", 
+      hours: db.list('/playground/' + this.today + '/1').valueChanges()
+    },{
+      id: 2,
+      name: "VUT", 
+      hours: db.list('/playground/' + this.today + '/2').valueChanges()
+    }];
   }
 }
