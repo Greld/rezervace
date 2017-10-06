bb47c7f7d5943afdad248b8e938b69ca0e98360a
Jan Kucera <Jan.Kucera@firma.seznam.cz>
2017-10-06 18:28:59 +0200

    Napojeni na Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// Firebase:
+// no struktura je DEN - HALA - HODINA  - KURTOGRUPA - OBSAZENO/CENA/a dalsi parametry
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+	apiKey: "AIzaSyCKKIwGD24wrWiOlp1Lj8h2pqj9X1b2tfI",
+    authDomain: "skymsk.firebaseapp.com",
+    databaseURL: "https://skymsk.firebaseio.com",
+    projectId: "firebase-skymsk",
+    storageBucket: "firebase-skymsk.appspot.com",
+    messagingSenderId: "701919856399"
+  }
 };
