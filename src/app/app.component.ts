2f17fed8d2b0d8304a4f85ca1eaa78b396daf5fd
Jan Kucera <Jan.Kucera@firma.seznam.cz>
2017-10-18 19:22:42 +0200

    do jednoho dataObserveru a do tabulky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,13 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  timeIsUp = false;
   title = 'Rezervace'; 
   today = new Date().toJSON().slice(0,10);
   arenas: [{
     arenaId: number,
     playgroundId: number,
     name: String,
-    skymUrl: String,
-    hours: Observable<any[]>
+    skymUrl: String
   }]; 
   hours = [];
   defaultDate: Date;
@@ -29,66 +27,59 @@ export class AppComponent {
     name: "Studenti"
   }];
   priceGroupId = this.priceGroups[0].id;
+  availabilityObserver: Observable<any>;
 
   constructor(db: AngularFireDatabase) {
     this.db = db;
     for (var i = 6; i < 23; i+=0.5) { this.hours.push(Math.floor(i).toString().padStart(2, "0") + ":" + ((i - Math.floor(i)) * 60).toString().padStart(2, "0"));}
-    this.setReservationData();    
-  }
-
-  setDate(date){
-    this.today = date;
-    this.setReservationData();
-  }
-
-  setReservationData(){
-    this.timeIsUp = false;
+    
     this.arenas = [{
       arenaId: 1,
       playgroundId: 1,
       name: "Sprint", 
-      skymUrl: "https://skym.cz/sportoviste/sprint/",
-      hours: this.db.list('/court-availability/' + this.today + '/1').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/sprint/"
     },{
       arenaId: 93,
       playgroundId: 98,
       name: "CESA VUT tělocvična Purkyňova", 
-      skymUrl: "https://skym.cz/sportoviste/cesa-vut-telocvicna-purkynova/",
-      hours: this.db.list('/court-availability/' + this.today + '/98').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/cesa-vut-telocvicna-purkynova/"
     },{
       arenaId: 94,
       playgroundId: 99,
       name: "CESA VUT - tělocvična F1", 
-      skymUrl: "https://skym.cz/sportoviste/cesa-vut-sa-ppv---telocvicna-f1/",
-      hours: this.db.list('/court-availability/' + this.today + '/99').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/cesa-vut-sa-ppv---telocvicna-f1/"
     },{
       arenaId: 35,
       playgroundId: 33,
       name: "Sportcentrum Kuklenská", 
-      skymUrl: "https://skym.cz/sportoviste/sportcentrum-kuklenska/",
-      hours: this.db.list('/court-availability/' + this.today + '/33').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/sportcentrum-kuklenska/"
     },{
       arenaId: 41,
       playgroundId: 44,
       name: "Badminton Židenice", 
-      skymUrl: "https://skym.cz/sportoviste/badminton-zidenice/",
-      hours: this.db.list('/court-availability/' + this.today + '/44').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/badminton-zidenice/"
     },{
       arenaId: 50,
       playgroundId: 54,
       name: "Yonex Jehnice", 
-      skymUrl: "https://skym.cz/sportoviste/yonex-jehnice/",
-      hours: this.db.list('/court-availability/' + this.today + '/54').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/yonex-jehnice/"
     },{
       arenaId: 38,
       playgroundId: 39,
       name: "Club Classic", 
-      skymUrl: "https://skym.cz/sportoviste/club-classic/badminton/",
-      hours: this.db.list('/court-availability/' + this.today + '/39').valueChanges()
+      skymUrl: "https://skym.cz/sportoviste/club-classic/badminton/"
     }];
-    setTimeout(() => {
-      this.timeIsUp = true;
-    }, 3000);
+
+    this.setReservationData();    
+  }
+
+  setDate(date) {
+    this.today = date;
+    this.setReservationData();
+  }
+
+  setReservationData() {
+    this.availabilityObserver = this.db.object('/court-availability/' + this.today).valueChanges();
   }
 
   getReservationUrl(arenaId, date) {
