11bfc6aebfd78bcf996ac5295e25b703f093ea42
Jan Kucera <Jan.Kucera@firma.seznam.cz>
2017-11-07 18:15:29 +0100

    nova komponenta do sve slozky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ReservationTableComponent } from './reservationTable.component';
+import { ReservationTableComponent } from './reservationTable/reservationTable.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/obsazenost', pathMatch: 'full' },  
